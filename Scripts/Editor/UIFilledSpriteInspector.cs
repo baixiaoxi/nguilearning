60ed871bf72e525a53da5e544cfefc7282f7987f
U-YSET1003\Administrator <Administrator@yset1003.(none)>
2013-05-25 15:01:32 +0800

    add ngui

---
+++ Scripts/Editor/UIFilledSpriteInspector.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Filled sprite is obsolete. It's only kept for backwards compatibility.
+/// </summary>
+
+[CustomEditor(typeof(UIFilledSprite))]
+public class UIFilledSpriteInspector : UISpriteInspector {}
